e428984223d2d1449beee0276b4e8509ad4aa334
manishnewprojects <manishnewprojects@gmail.com>
2020-05-17 10:21:30 -0700

    Update webcmg.py for hacked email

--- webcmg.py
+++ webcmg.py
@@ -24,8 +24,8 @@ app.config["MAIL_SERVER"] = "smtp.office365.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = 'info@webcmg.com'
-app.config["MAIL_PASSWORD"] = '120+Skipper'
+app.config["MAIL_USERNAME"] = ''
+app.config["MAIL_PASSWORD"] = ''
  
 mail.init_app(app)
      
